a8c023509df5bd2d5ca3d34455cf25359d6700f8
serper35 <serper.wolf@gmail.com>
2023-08-20 18:01:30 +0300

    solved

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -5,8 +5,19 @@ import java.util.Scanner;
 public class ElectronicWatch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int seconds = scanner.nextInt();
+        long seconds = scanner.nextInt();
+        long minutes = seconds / 60 % 60;
+        long hours = seconds / 3600;
+        seconds %= 60;
+        if (hours < 23) {
+            String timeInHHMMSS = String.format("%2d:%02d:%02d", hours, minutes, seconds);
+            System.out.println(timeInHHMMSS);
 
-
+        }
+        else {
+            hours = hours % 24;
+            String timeInHHMMSS = String.format("%02d:%02d", minutes, seconds);
+            System.out.println(hours + ":" + timeInHHMMSS);
+        }
     }
 }
